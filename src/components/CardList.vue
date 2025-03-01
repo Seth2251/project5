a0224c3449b4f8b188e494e8d30580a8f7b0792e
Seth Davila <sdavil15@stu.southtexascollege.edu>
2025-03-01 16:49:12 -0600

    Creating CardList

---
+++ src/components/CardList.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-6 p-6">
+    <Card v-for="index in 12" :key="index" />
+  </div>
+</template>
+
+<script setup>
+import Card from "./Card.vue";
+</script>
