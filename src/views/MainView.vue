a78dade8fd5c74de6befa711c7563a8a100b19cc
Seth Davila <sdavil15@stu.southtexascollege.edu>
2025-03-01 16:57:40 -0600

    creating the Main veiw

---
+++ src/views/MainView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1 class="text-center text-3xl font-bold my-6">🎬 Random Movie Generator</h1>
+    <CardList />
+  </div>
+</template>
+
+<script setup>
+import CardList from "@/components/CardList.vue";
+</script>
